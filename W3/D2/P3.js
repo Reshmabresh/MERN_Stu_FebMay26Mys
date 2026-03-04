124426cb27ff397c0067d48267fd182d87901131
Reshmabresh <rreshmab113@gmail.com>
2026-03-04 23:46:03 +0530

    logical operators

---
+++ W3/D2/P3.js
@@ -0,0 +1,6 @@
+//logical operators
+console.log("logical operators");
+let a = true , b = false;
+console.log("a&&b= ",a&&b); //logical AND
+console.log("a||b=",a||b); //Logical OR
+console.log("!a",!a); //Logical NOT
