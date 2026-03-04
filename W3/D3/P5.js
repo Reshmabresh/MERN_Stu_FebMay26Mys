0cdfc72be954112141dc83e5d1812423bc3950c8
Reshmabresh <rreshmab113@gmail.com>
2026-03-04 12:39:30 +0530

    Basics of object

---
+++ W3/D3/P5.js
@@ -0,0 +1,9 @@
+//Basics of object
+const person = {
+    name:"Reshma",
+    age:22,
+    isStudent: false
+};
+//console.log("Person",person);
+console.log("Name",person.name);
+console.log("Age",person["age"]);
