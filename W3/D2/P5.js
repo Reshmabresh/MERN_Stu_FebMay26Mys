2c60b989a639312d447ca03e45d7e538bb073ac2
Reshmabresh <rreshmab113@gmail.com>
2026-03-04 23:47:26 +0530

    Function declaration

---
+++ W3/D2/P5.js
@@ -0,0 +1,9 @@
+//Function declaration
+//function greet(name);
+greet("Reshma");
+// function declaration/definition
+function greet(name){
+    console.log("Hello, "+name+" !");
+}
+// Function call
+greet("Reshma");
