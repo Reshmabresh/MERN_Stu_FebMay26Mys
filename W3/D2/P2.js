809bf91ffcce8e88f036a68b474718f84f7c27d8
Reshmabresh <rreshmab113@gmail.com>
2026-03-04 23:45:33 +0530

    Comparison operators

---
+++ W3/D2/P2.js
@@ -0,0 +1,11 @@
+// Comparison operators
+console.log("Comparison operators");
+const x = 3, y = 4;
+console.log("x==y: ",x==y);// Loose equality
+console.log("x===y: ",x===y);// Strict equality
+console.log("x!=y: ",x!=y);// Loose inequality
+console.log("x!==y ",x!==y);// Strict equality
+console.log("x>3: ",x>3);
+console.log("x<3: ",x<3);
+console.log("x>=3: ",x<=3);
+console.log("x<=3: ",x<=3);
