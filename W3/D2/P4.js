68947d3f13252f18ab603eb5f693ba535e225368
Reshmabresh <rreshmab113@gmail.com>
2026-03-04 23:46:49 +0530

    Conditional statements

---
+++ W3/D2/P4.js
@@ -0,0 +1,33 @@
+//Conditional statements
+let age = 20;
+if(age < 13){
+    console.log("Child");
+}
+else if(age<18){
+    console.log("Teenager");
+}
+else
+{
+    console.log("Adult");
+}
+
+// Switch statements
+console.log("Switch statements");
+let day = "Tuesday";
+switch (day) {
+    case "Monday":
+        console.log("Start of the week");
+        break;
+    case "Wednesday":
+        console.log("Mid of the week");
+        break;
+    case "Friday":
+        console.log("End of the week");
+        break;
+        default:
+            console.log("Some day in the week");
+        break;
+}
+// Type conversion
+let n = Number("ABC");
+console.log("n: ",n,"Type of n: ",typeof n, "isNaN",isNaN(n));
