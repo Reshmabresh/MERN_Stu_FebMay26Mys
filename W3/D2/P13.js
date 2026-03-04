928998e704a2d5681e8bbeee0d50012b4347a055
Reshmabresh <rreshmab113@gmail.com>
2026-03-04 23:52:44 +0530

    Function scope

---
+++ W3/D2/P13.js
@@ -0,0 +1,23 @@
+// Function scope
+function scope(){
+    var insideVar = 10;
+    let insidelet = 20;
+    const insideConst = 30;
+    //console.log(insideVar);
+    //console.log(insidelet);
+    //console.log(insideConst);
+}
+scope();
+//console.log(insideVar);//Function scoped
+//console.log(insidelet);//block scoped
+//console.log(insideConst);//block scoped
+
+function varFunctionScoped(){
+    if(true){
+        var x = 40;
+        let y = 50;
+    }
+    console.log("x: ",x);
+    console.log("y: ",y);
+}
+varFunctionScoped();
