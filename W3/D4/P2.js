faef14679c7078ff2c0270b9914a14d719264e12
Reshmabresh <rreshmab113@gmail.com>
2026-03-05 10:55:53 +0530

    Array Iteration

---
+++ W3/D4/P2.js
@@ -0,0 +1,15 @@
+//Array Iteration
+console.log("Array Iteration");
+let numArray = [1, 2, 3, 4];
+// for loop
+for(let i = 0; i < numArray.length; i++){
+    console.log(" ",numArray[i]);
+}
+//for..of Loop
+for(let num of numArray){
+    console.log(" ", num);
+}
+//forEach method
+numArray.forEach((val,idx) => {
+    console.log(" ",idx, val);
+})
