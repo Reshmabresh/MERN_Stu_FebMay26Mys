6676ab4d4d38a3d4b89ae23eabebe2473dd34842
Reshmabresh <rreshmab113@gmail.com>
2026-03-08 19:18:48 +0530

    Basics of click events (JS)

---
+++ W3/D5/P1.js
@@ -0,0 +1,8 @@
+//Js: Basics of click events
+
+const button = document.getElementById("clickBtn");
+button.addEventListener("click", function() {
+});
+button.addEventListener("click", function() {
+    console.log("Second event listener: Button is clicked");
+});
