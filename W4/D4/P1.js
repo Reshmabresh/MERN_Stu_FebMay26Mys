371f4be795bb1edd66a67a0ec46f309566c7c6c9
Reshmabresh <rreshmab113@gmail.com>
2026-03-12 15:28:44 +0530

    Introduction to Encoding

--- W4/D4/P1.js
+++ W4/D4/P1.js
@@ -1,20 +1,16 @@
 const out = document.getElementById("out");
 const inspectBtn = document.getElementById("inspectBtn");
 
-inspectBtn.addEventListener("click",
-    function(){
-        const info = {
-            htmlLang : document.documentElement.lang,
-            charset : document.characterSet,
-            //browser's primary preferred language 
-            browserLanguage : navigator.language,
-            //Array of all browser language
-            browserLanguages : navigator.languages,
-            sampleText : document.getElementById("sampleText")
-            .textContent
-        };
-        console.log(info);
-        out.textContent = JSON.stringify(info,null,2);
-
-    }
-);
+inspectBtn.addEventListener("click",function(){
+    const info = {
+        htmlLang : document.documentElement.lang,
+        charset: document.characterSet,
+        //browser's primary prefered langvage
+        browserLangvage : navigator.language,
+        //Array of all browsered langvage
+        browserLangvages : navigator.languages,
+        sampleText : document.getElementById("sampleText").textContent
+    };
+    console.log(info);
+    out.textContent = JSON.stringify(info,null,2);
+});
