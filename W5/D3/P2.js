17f52e713e8e65b3bf42fc1a0272a3c697aee7dc
Reshmabresh <rreshmab113@gmail.com>
2026-03-18 10:41:34 +0530

    use of async/await

--- W5/D3/P2.js
+++ W5/D3/P2.js
@@ -2,7 +2,26 @@
 function getUser(){
     return new Promise(function(resolve){
         setTimeout(function(){
-            resolve({id:101,name:"Kiran"});
+            resolve({id:101,name:"Ahalya"});
         },1000);
     });
 }
+
+function getOrders(userId){
+    return new Promise(function (resolve){
+        setTimeout(function(){
+            resolve(["order-A","order-B"]);
+        },1200);
+    });
+}
+
+async function showUserAndOrders(){
+    const user = await getUser();
+    console.log("User loaded: ",user.name);
+
+    const orders = getOrders(user.id);
+    console.log("Orders loaded",orders);
+    
+}
+
+showUserAndOrders();
