fba7b01e0dd110b54fb7bdd08075033260a980fb
Reshmabresh <rreshmab113@gmail.com>
2026-03-24 16:28:53 +0530

    commonJS export and import

---
+++ W6/D1/P2.js
@@ -0,0 +1,14 @@
+//commonJS export and import
+
+function calculateTax(amount){ // amount is the input
+    return amount*0.18;     //0.18 is tax 18%
+}
+
+function applyDiscount(amount,percent){
+    return amount - amount * (percent/100);
+}
+function formatCurrency(amount){
+    return "INR " +amount.toFixed(2);
+}
+//module.exports makes these functions available to require()
+module.exports = {calculateTax,applyDiscount,formatCurrency};
