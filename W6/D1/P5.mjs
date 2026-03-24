c1abe62aeffc26388c4a77e3cdd1794358efa4db
Reshmabresh <rreshmab113@gmail.com>
2026-03-24 16:30:23 +0530

    ES module exports

---
+++ W6/D1/P5.mjs
@@ -0,0 +1,17 @@
+// ES module exports
+
+//2 types of export: Named & Default export
+//Named export for a shared constant
+// Named export can be of any number in a file
+export const taxrate = 0.18;
+
+//Named export for a reusable function
+export function calculateTotal(subtotal){
+   return subtotal + subtotal*taxrate;
+}
+
+//Default export : for the main feature of the module
+// can have only one per file
+export default function createInvoiceLabel(invoiceNumber){
+    return "Invoice: "+invoiceNumber;
+}
