a68da080fafd052bea55fb1b436316ba42948e4b
Reshmabresh <rreshmab113@gmail.com>
2026-03-25 10:07:33 +0530

    Reading & writeing files synchronously

---
+++ W6/D3/P2.js
@@ -0,0 +1,16 @@
+// Reading & writeing files synchronously
+
+const fs = require("fs");
+const path = require("path");
+
+const filePath = path.join(__dirname,"sync-note.txt");
+//Syntax for function usage in a module
+//moduleName.function name() 
+
+fs.writeFileSync(filePath,"This file was written using writeFileSync().\nSynchronous operation block execution");
+console.log("File written synchronously.");
+
+const content = fs.readFileSync(filePath,"utf-8");
+
+console.log("File read synchronously.");
+console.log(content);
