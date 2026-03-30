eb7c8d7886d44dabae37328b5d00aed9eb7991d3
Reshmabresh <rreshmab113@gmail.com>
2026-03-30 11:04:43 +0530

    Basic of ExpressJS - setup

---
+++ W7/D1/p1.js
@@ -0,0 +1,22 @@
+// Basic of ExpressJS - setup
+// npm init -y
+// npm install express
+
+//Import module of Express
+const express = require("express");
+
+//calling express() creates the main application object
+//This object is used to register routes and middleware
+const app = express();
+
+//app.get() handles GET requestes for a specific path
+app.get("/",function(req,res){
+    //res.send() sends a responses body and 
+    //ends the request automatically
+    res.send("Hello from express server");
+});
+
+//listen() starts the server on a chosen port number
+app.listen(4000,function(){
+    console.log("Express server running at http://localhost:4000");
+});
