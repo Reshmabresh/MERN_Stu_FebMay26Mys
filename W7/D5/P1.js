7e689ccd19bbf5e534b43ad385e87cf83e682c6d
Reshmabresh <rreshmab113@gmail.com>
2026-04-03 11:13:43 +0530

    Cookie is used to track session ID

---
+++ W7/D5/P1.js
@@ -0,0 +1,16 @@
+// Cookie is used to track session ID
+
+//Simulated sever-side session store 
+const sessionStore = {
+    "abc123":{
+        userId: 101,
+        username: "Ahalya",
+        role:"student"
+    }
+};
+//Simulated browser cookie value
+const browerCookieSessionId = "abc123";
+
+
+const sessionData = sessionStore[browerCookieSessionId]
+console.log("Server-side session data:",sessionData);
