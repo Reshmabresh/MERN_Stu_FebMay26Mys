e1125ec461ff018e071dc57025043640ffdc9bd0
Reshmabresh <rreshmab113@gmail.com>
2026-04-03 11:32:21 +0530

    Managing session with express-session

---
+++ W7/D5/P2.js
@@ -0,0 +1,42 @@
+// Managing session with express-session 
+
+const express = require("express");
+const session = require("express-session");
+
+const app = express();
+
+app.use(session({
+    secret:"MySecretKey",
+    resave: false,
+    saveUninitialized:false,
+    cookie: {
+        maxAge: 60 * 60 * 1000
+    }
+}));
+
+app.get("/login",function(req,res){
+    req.session.user = {
+        id:201,
+        username: "Ahalya",
+        role: "student"
+    };
+    res.send("Session details stored after login.");
+});
+
+app.get("/profile",function(req,res){
+    if(!req.session.user){
+        return res.status(401).json({
+            success:false,
+            message:"No active login session found."
+        });
+    
+    }
+    res.json({
+        success:true,
+        sessionUser: req.session.user
+    });
+});
+
+app.listen(4000,function(){
+    console.log("JWT demo server running @ http://localhost:4000");
+});
