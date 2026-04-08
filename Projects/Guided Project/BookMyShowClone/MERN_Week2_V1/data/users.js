9fef1f53cc84cee9ac37ec25c3ddccc1793c96be
Reshmabresh <rreshmab113@gmail.com>
2026-04-08 10:07:10 +0530

    User details of the application

---
+++ Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/data/users.js
@@ -0,0 +1,17 @@
+// User details of the application
+const users = [
+    {
+        id:1,
+        name:"Ahalya",
+        email:"u@e.com",
+        password:"mahi123",
+        role:"user"
+    },
+    {
+        id:2,
+        name:"Admin",
+        email:"A@e.com",
+        password:"admin123",
+        role:"admin"
+    }
+]
