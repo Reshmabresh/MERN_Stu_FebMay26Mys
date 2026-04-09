d1dadad113cc24257db8dd60444a346b140f8060
Reshmabresh <rreshmab113@gmail.com>
2026-04-09 12:06:14 +0530

    User Login, profile fetch and logout of user functionality created

--- Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/controllers/authController.js
+++ Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/controllers/authController.js
@@ -2,7 +2,7 @@
 const jwt = require("jsonwebtoken");
 const users = require("../data/users");
 
-const CustomError = require("../utils/CustomError");
+const CustomError = require("../utils/customError");
 
 const {JWT_SECRET} = require("../middleware/authMiddleware");
 
