4a74123c231c8926260c75f4e8a38ec0505ee0ad
Reshmabresh <rreshmab113@gmail.com>
2026-05-04 11:55:55 +0530

    updating objects and arrays

---
+++ W10/D1/state-hooks-app/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { UseStateBasics } from "./components/P1";
+import { UpdatingObjectsArraysState } from "./components/P2";
+function App() {
+  return (
+    <>
+      {/* <UseStateBasics /> */}
+      <UpdatingObjectsArraysState />
+    </>
+  )
+}
+export default App;
