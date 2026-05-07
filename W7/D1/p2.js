6062ec88819a00a7b2a0a4c95fb4569983a86b67
Reshmabresh <rreshmab113@gmail.com>
2026-05-07 18:59:42 +0530

    Basic routing in Express

--- W7/D1/p2.js
+++ W7/D1/p2.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 
 app.get("/",function(req,res){
-    res.send("Hello from express server");
+    res.send("Home route in express server");
 });
 
 app.get("/about",function(req,res){
@@ -12,9 +12,9 @@ app.get("/about",function(req,res){
 });
 
 app.get("/products",function(req,res){
-    res.send("Products rout in express server");
+    res.send("Products route in express server");
 });
 
 app.listen(4000,function(){
     console.log("Express server running at http://localhost:4000");
-})
+});
