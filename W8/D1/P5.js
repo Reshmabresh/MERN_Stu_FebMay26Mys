2924ad2fdd4b5d61d7d4bdca1ef552c6df37e3f0
Reshmabresh <rreshmab113@gmail.com>
2026-05-07 22:00:31 +0530

    Aggreagation functions in MongoDB

--- W8/D1/P5.js
+++ W8/D1/P5.js
@@ -1,44 +1,51 @@
-// Aggregation function in mongoDB
+// Aggreagation functions in MongoDB
 const mongoose = require("mongoose");
-async function runAggregate() {
-    try {
+async function runAggregate(){
+    try{
         await mongoose.connect("mongodb://localhost:27017/merntraining");
         console.log("MongoDB connected successfully");
 
         const saleSchema = new mongoose.Schema({
-            category: String,
-            amount: Number,
-            status: String
+            category:String,
+            amount:Number,
+            status:String
         });
-         const Sale = mongoose.models.Sale || mongoose.model("Sale", saleSchema);
-        await Sale.deleteMany({ status: "demo-sale" });
-        await Sale.create([
-            { category: "books", amount: 200, status: "demo-sale" },
-            { category: "books", amount: 300, status: "demo-sale" },
-            { category: "books", amount: 400, status: "demo-sale" },
-            { category: "Stationary", amount: 150, status: "demo-sale" },
-            { category: "Stationary", amount: 200, status: "demo-sale" },
-            { category: "Stationary", amount: 350, status: "demo-sale" },
-        ]);
+
+        const Sale = mongoose.models.Sale || mongoose.model("Sale",saleSchema);
+       
+        // await Sale.deleteMany({status:"demo-sale"});
+
+        // await Sale.create([
+        //     {category:"books", amount: 200, status:"demo-sale"},
+        //     {category:"books", amount: 300, status:"demo-sale"},
+        //     {category:"books", amount: 400, status:"demo-sale"},
+        //     {category:"stationary", amount: 100, status:"demo-sale"},
+        //     {category:"stationary", amount: 150, status:"demo-sale"},
+        //     {category:"stationary", amount: 450, status:"demo-sale"}
+        // ]);
+
         const aggregationResult = await Sale.aggregate([
-          //$match filters the inconing documents first
+            // $match filters the incoming documents first
             {$match: {status:"demo-sale"}},
-            //$group: groups by a specfic condition
+
+            // $group: groups by a specific condition & calculate total & average
             {$group:{
                 _id:"$category",
-                totalAmount:{$sum:"$amount"},
-                averageAmount:{$avg:"$amount"},
-                itemCount:{$sum:1}
+                totalAmount: {$sum:"$amount"},
+                averageAmount: {$avg:"$amount"},
+                itemCount: {$sum:1}
             }},
-            //$sort orders the grouped ouput
+            // $sort orders the grouped output
             {$sort:{totalAmount:-1}}
         ]);
         console.log("Aggregation result:",aggregationResult);
+
         await mongoose.connection.close();
         console.log("connection closed");
     }
-    catch (error) {
-    console.log("Filter demo error:", error.message);
+    catch(error){
+        console.log("Aggregate demo error:",error.message);
+    }
 }
-}
+
 runAggregate();
