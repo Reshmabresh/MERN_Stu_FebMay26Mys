21209bc6c91e55ac87fd140ff2228b75b594321c
Reshmabresh <rreshmab113@gmail.com>
2026-05-13 16:10:54 +0530

    Multiple Dynamic Params

---
+++ W11/D2/my-app/src/components/P3.jsx
@@ -0,0 +1,14 @@
+// Multiple Dynamic Params
+import { useParams } from "react-router-dom"
+
+export function MultipleDynamicParams(){
+    const { userId, orderId } = useParams();
+
+    return(
+        <div>
+            <h2>Multiple Dynamic Params</h2>
+            <p>User Id from URL : {userId}</p>
+            <p>Order Id from UPL : {orderId}</p>
+        </div>
+    );
+}
